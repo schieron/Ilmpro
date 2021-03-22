1fa76bbac0a73ffd930f179ef6d66502aefd2bb2
schieron <63161902+schieron@users.noreply.github.com>
2021-03-22 11:04:02 +0100

    Coliedes

---
+++ Assets/JPG/Muppets_World_Tour_Stage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Muppets_World_Tour_Stage" tilewidth="32" tileheight="32" tilecount="8850" columns="118">
+ <image source="../Godot/Godot_Ilmpro/Assets/JPG/Muppets_World_Tour_Stage.webp" width="3800" height="2400"/>
+</tileset>
