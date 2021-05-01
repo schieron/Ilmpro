7e71a510fdb1a75f37a31994fd0b2ca9ec70dbd4
schieron <schieron@web.de>
2021-05-01 02:24:50 +0200

    ment

--- deploy/setAudioStream.sh
+++ deploy/setAudioStream.sh
@@ -4,17 +4,22 @@ set -e
 declare -A STREAM_DAY_URLS
 declare -A STREAM_NIGHT_URLS
 declare STREAM_URL
+declare STREAM_MINP_URL
+declare STREAM_MOUT_URL
+declare STREAM_MINF_URL
 TZ=Europe/Berlin
 export TZ
 
 
 STREAM_LIVE_URL="https://bit.ly/3vCuXxB"
-MENTI_URLS[0]="https://bit.ly/3fpaSpv"
-MENTI_URLS[1]="https://bit.ly/3fpaSpv"
+MENTI_URLS[0]="https://www.menti.com/ubrf586ufg/"
+MENTI_URLS[1]="https://bit.ly/3ualdKD"
 MENTI_URLS[2]="https://bit.ly/3fpaSpv"
 
 PLACEHOLDER="--streamurl--"
-
+PLACEHOLDER1="--streaminpurl--"
+PLACEHOLDER2="--streamouturl--"
+PLACEHOLDER3="--streaminfurl--"
 
 live=1
 
@@ -24,5 +29,11 @@ fi
 
 echo "Stream URL: $STREAM_URL"
 
+STREAM_MINP_URL=$(sed 's|/|\\/|g' <<< ${MENTI_URLS[0]})
+STREAM_MOUT_URL=$(sed 's|/|\\/|g' <<< ${MENTI_URLS[1]})
+STREAM_MINF_URL=$(sed 's|/|\\/|g' <<< ${MENTI_URLS[2]})
 
 sed -i  "s/$PLACEHOLDER/$STREAM_URL/g" ./*.json
+sed -i  "s/$PLACEHOLDER1/$STREAM_MINP_URL/g" ./*.json
+sed -i  "s/$PLACEHOLDER2/$STREAM_MOUT_URL/g" ./*.json
+sed -i  "s/$PLACEHOLDER3/$STREAM_MINF_URL/g" ./*.json
