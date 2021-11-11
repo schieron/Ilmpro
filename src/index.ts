8544bd25723fb6de95bcc0afc6e964308bf48c13
schieron <63161902+schieron@users.noreply.github.com>
2021-11-11 22:46:41 +0000

    Deploying to gh-pages from master @ a09c6bb1174a1c1089db1986b072e0a8129c5f48 🚀

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+import { Sound } from "@workadventure/iframe-api-typings/Api/iframe/Sound/Sound";
+import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+
+// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+bootstrapExtra().catch(e => console.error(e));
+
+let currentPopup: any = undefined;
+
+let playAuftritt: any = undefined;
+
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes();
+
+WA.room.onEnterZone('clock', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+})
+
+WA.room.onLeaveZone('clock', closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+//let stream = "http://ilmprokombinat.ddns.net:8000/ilmpro"
+let localstream = "http://192.168.178.40:8000/ilmpro"
+
+WA.room.onEnterZone('life', () => {
+    playAuftritt =  WA.sound.loadSound(localstream);
+    var config = {
+        volume : 0.5,
+        loop : false,
+        rate : 1,
+        detune : 1,
+        delay : 0,
+        seek : 0,
+        mute : false
+    }
+    playAuftritt.play(config);
+});
+
+//mySound.stop();
