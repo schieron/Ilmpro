4d28565b990e7b146bc4835e7b68256d8811a5bd
schieron <63161902+schieron@users.noreply.github.com>
2022-09-27 14:41:27 +0000

    Deploying to gh-pages from master @ 801c49f6812666561bbeb03f2f3b0eef47e703ee 🚀

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@
 import { Sound } from "@workadventure/iframe-api-typings/Api/iframe/Sound/Sound";
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
+console.log('Skript is Staten');
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+bootstrapExtra().then(() =>{
+    console.log('API is runnig');
+}).catch(e => console.error(e));
 
 let currentPopup: any = undefined;
-
 let playAuftritt: any = undefined;
-
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
@@ -19,19 +20,13 @@ WA.room.onEnterZone('clock', () => {
 
 WA.room.onLeaveZone('clock', closePopUp)
 
+
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
 }
-
-
-WA.room.onEnterZone('door', () => {
-    currentPopup =  WA.ui.openPopup("door","Enter The Password",[]);
-})
-
-WA.room.onLeaveZone('door', closePopUp)
 
 
 
@@ -68,3 +63,31 @@ function stopStream(){
     }
     playAuftritt.stop();
 }
+
+/*
+WA.onInit().then(() => {
+    console.log("Scripting API Ok");
+    WA.room.onEnterLayer(layername: 'door').subscribe(next : () => {
+       (WA.state.voteA as number)++;
+      //  currentPopup =  WA.ui.openPopup("door","Enter The Password",[]);
+      //  WA.chat.sendChatMessage("Hello!", "Mr Robot");
+   })
+}).catch(e => console.error(e));
+    
+
+WA.room.onEnterLayer('myLayer').subscribe(() => {
+    WA.chat.sendChatMessage("Hello!", 'Woka');
+});
+
+WA.room.onLeaveLayer('myLayer').subscribe(() => {
+    WA.chat.sendChatMessage("Goodbye!", 'Woka');
+});
+
+/*
+WA.room.onEnterZone('door', () => {
+    currentPopup =  WA.ui.openPopup("door","Enter The Password",[]);
+})
+*/
+
+//WA.room.onLeaveZone('door', closePopUp)
+
